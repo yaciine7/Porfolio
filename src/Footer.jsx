bc4cb445fa8495c0d4d10bd4df2a7d954746db97
Yacine kecir <yacinekecir63@gmail.com>
2026-08-25 21:13:06 +0100

    fixing

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <>
-      <p className="text-center text-white mt-70 pb-7 font-bold">&copy; {new Date().getFullYear()} Yacine. All rights reserved.</p>
+      <p className="text-center mb-0 text-white mt-70 pb-7 font-bold">&copy; {new Date().getFullYear()} Yacine. All rights reserved.</p>
     </>
   );
 }
